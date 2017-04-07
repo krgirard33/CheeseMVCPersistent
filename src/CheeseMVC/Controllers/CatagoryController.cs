5f4ec6bb7b8339da5dfead1c93dcb040e292961a
krgirard33 <ken.girard@gmail.com>
2017-04-06 19:29:27 -0500

    Created CatagoryController, CheeseDbContext, & CheeseCatagory

---
+++ src/CheeseMVC/Controllers/CatagoryController.cs
@@ -0,0 +1,17 @@
+﻿using CheeseMVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Controllers
+{
+    public class CatagoryController
+    {
+        private readonly CheeseDbContext context;
+        public CatagoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+    }
+}
